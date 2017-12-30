1fca4a86d3a6a3d8315752451c271caa58c11344
Antonin Rousset <AntoninRousset@users.noreply.github.com>
2017-12-30 13:31:44 +0100

    bitrates

--- makeWebm.sh
+++ makeWebm.sh
@@ -3,23 +3,28 @@
 # 4chan webm maker
 
 START_TIME="0:00:00"
-DURATION="120" # Max 120s
-MAX_SIZE="3M" # 3072KB
-COMPRESSION="10" # [4, 63]
-AUDIO_QUALITY="3"
+DURATION="60" # Max 120s
+#MAX_SIZE="3145728" # 3MB 
+MAX_SIZE="4194304" # 4MB
+RESOLUTION="720" #720p
 
-ffmpeg -i "$1" -ss "$START_TIME" -t "$DURATION" -vcodec libvpx -crf "$COMPRESSION" -acodec libvorbis -aq "$AUDIO_QUALITY" -fs "$MAX_SIZE" "$2" -pass1
-ffmpeg -i "$1" -ss "$START_TIME" -t "$DURATION" -vcodec libvpx -crf "$COMPRESSION" -acodec libvorbis -aq "$AUDIO_QUALITY" -fs "$MAX_SIZE" "$2" -pass2
+in="$1"
+out="$2"
+shift 2
+
+a_bitrate="96000"
+v_bitrate="$((8*$MAX_SIZE/$DURATION-$a_bitrate))"
+
+ffmpeg -i "$in" -ss "$START_TIME" -t "$DURATION" -map_metadata -1 -c:v libvpx -b:v "${v_bitrate}" -vf scale=-1:"$RESOLUTION" -c:a libvorbis -b:a "$a_bitrate" -quality best "$out" -pass 1 $@ &&\
+ffmpeg -i "$in" -ss "$START_TIME" -t "$DURATION" -map_metadata -1 -c:v libvpx -b:v "${v_bitrate}" -vf scale=-1:"$RESOLUTION" -c:a libvorbis -b:a "$a_bitrate" -quality best "$out" -pass 2 $@ -y
 
 
 # TODO
 # OPENCL/MULTITHREADING
 # MAX_DIMENSIONS of 2048x2048
-# AUTOMATICALLY SET COMPRESSION
 # -to
 # TOGGLE AUDIO WITH -an (/b/)
 # CHECK -threads
 # CHECK allowed for 4chan
 # youtube-dl
 # automatically $2 from $1 if not defined
-# bitrate
